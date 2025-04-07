fd85a347ed96026c39a2d5e15ca865b48fb30770
robin-peter <rpeter@berkeley.edu>
2025-04-07 10:14:34 -0700

    Add Ce-134 / La-134 and fix typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import radioactivedecay as rd
 import numpy as np
 import plotly.graph_objects as go
-import pandas as pd
 import datetime
 
 # TODO:
@@ -18,9 +17,11 @@ def parse_time(time_str):
     except ValueError:
         st.error("Invalid time format. Please enter time as HH:MM.")
         return None
-    
+
+
 def parse_nuclide(chain_str):
     return chain_str.split(" / ")
+
 
 # Title and information
 st.title("Radioactive Decay and Ingrowth")
@@ -32,19 +33,22 @@ st.markdown("""
     2. Fill in the initial inventory amount of each isotope. This could be a measurement or, e.g., the vendor-provided values. Also provide the date and time of this measurement. (These values are not required if you only want to look at relative decay.)
     3. Hover over the figure to see the decay and ingrowth of the isotopes over time.
     4. Activity at specific time points can be calculated below. You can select a date/time combination or manually input a decay time.
-        * The units correspond to the Figure time units, which you can change in the drop-down menus.
+       * The units correspond to the Figure time units, which you can change in the drop-down menus.
 
     For suggestions or assistance, please contact me (https://github.com/robin-peter/rad-decay-app).
 """)
 
 # Dropdown
 radioisotopes = ["Th-227 / Ra-223",
-                 "Ac-225 / Bi-213"]
+                 "Ac-225 / Bi-213",
+                 "Ce-134 / La-134"]
 # more can be added in future versions
-radioisotope = st.selectbox("Select a Radioisotope:", options=list(radioisotopes))
+radioisotope = st.selectbox(
+    "Select a Radioisotope:", options=list(radioisotopes))
 a_units = ["uCi", "mCi", "Ci", "Bq", "kBq", "MBq"]
 t_units = ["d", "h", "m", "s"]
-t_factors = [(3600 * 24), 3600, 60, 1] # factor by which to convert s to the time unit above
+# factor by which to convert s to the time unit above
+t_factors = [(3600 * 24), 3600, 60, 1]
 
 nuc_p, nuc_d = parse_nuclide(radioisotope)
 
@@ -52,11 +56,14 @@ nuc_p, nuc_d = parse_nuclide(radioisotope)
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    Aa0 = st.number_input(f"Initial {nuc_p} activity:", min_value=0.0, max_value=200.0, value=1.0, step=0.1, format="%.3f")
-    selected_date = st.date_input("Date/time of initial activity:", datetime.date.today())
-    
+    Aa0 = st.number_input(f"Initial {nuc_p} activity:", min_value=0.0,
+                          max_value=200.0, value=1.0, step=0.1, format="%.3f")
+    selected_date = st.date_input(
+        "Date/time of initial activity:", datetime.date.today())
+
 with col2:
-    Ab0 = st.number_input(f"Initial {nuc_d} activity:", min_value=0.0, max_value=200.0, value=0.0, step=0.1, format="%.3f")
+    Ab0 = st.number_input(f"Initial {nuc_d} activity:", min_value=0.0,
+                          max_value=200.0, value=0.0, step=0.1, format="%.3f")
     time_input = st.text_input("(HH:MM, 24-hour time)", "12:00")
 
 with col3:
@@ -74,9 +81,11 @@ thalf_d = rd.Nuclide(nuc_d).half_life() / t_factor
 max_value = 12*thalf_d
 
 # Use radioactive decay package (which uses Bateman eqns)
-inv = rd.Inventory({nuc_p: Aa0 , nuc_d: Ab0 }, units=A_unit)
-df = inv.decay_time_series_pandas(time_period=max_value, time_units=t_unit, decay_units=A_unit)
-df = df[[nuc_p, nuc_d]] # select only relevant isotopes (from among whole chain)
+inv = rd.Inventory({nuc_p: Aa0, nuc_d: Ab0}, units=A_unit)
+df = inv.decay_time_series_pandas(
+    time_period=max_value, time_units=t_unit, decay_units=A_unit)
+# select only relevant isotopes (from among whole chain)
+df = df[[nuc_p, nuc_d]]
 
 # Plot figure
 fig = go.Figure()
@@ -105,10 +114,11 @@ if d1 and t:
     t = parse_time(t)
     t1 = datetime.datetime.combine(d1, t)
     dt = min(np.abs(t0 - t1), np.abs(-(t0 - t1)))
-    dt = dt.total_seconds() / 3600 / 24 # d
+    dt = dt.total_seconds() / 3600 / 24  # d
     x = dt
 
-x = st.number_input(f"Or specify time point manually ({t_unit}):", min_value=0.0, max_value=max_value, step=0.1, value=dt)
+x = st.number_input(
+    f"Or specify time point manually ({t_unit}):", min_value=0.0, max_value=max_value, step=0.1, value=dt)
 
 y1 = np.interp(x, df.index, df[nuc_p])
 y2 = np.interp(x, df.index, df[nuc_d])
@@ -116,12 +126,9 @@ y2 = np.interp(x, df.index, df[nuc_d])
 y1_pct = round(y1 / (y1+y2) * 100, 1)
 y2_pct = round(y2 / (y1+y2) * 100, 1)
 
-# Display the y values at the selected x value
+# Display the calculated values
 st.write(f"Activity distribution after {x} {t_unit}:")
-# st.write(f"  Th-227 activity : {y1 :.2f} uCi ({y1_pct :.1f}%)")
-# st.write(f"  Ra-223 activity : {y2 :.2f} uCi ({y2_pct :.1f}%)")
 
-# I like the default number formatting more so do rounding instead of in-line
 y1 = round(y1, 2)
 y2 = round(y2, 2)
 
